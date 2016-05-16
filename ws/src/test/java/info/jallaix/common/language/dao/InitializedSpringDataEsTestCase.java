d627abe8f425472252bd390c87239055fc1d96f8
Julien Allaix <jallaix@yahoo.fr>
2016-05-16 10:44:22 +0200

    InitializedSpringDataEsTestCase class refactoring

--- ws/src/test/java/info/jallaix/common/language/dao/InitializedSpringDataEsTestCase.java
+++ ws/src/test/java/info/jallaix/common/language/dao/InitializedSpringDataEsTestCase.java
@@ -1,8 +1,5 @@
 package info.jallaix.common.language.dao;
 
-import com.carrotsearch.randomizedtesting.RandomizedContext;
-import com.carrotsearch.randomizedtesting.RandomizedRunner;
-import com.carrotsearch.randomizedtesting.generators.RandomStrings;
 import com.github.tlrx.elasticsearch.test.EsSetup;
 import com.github.tlrx.elasticsearch.test.EsSetupRuntimeException;
 import com.github.tlrx.elasticsearch.test.provider.JSONProvider;
@@ -12,19 +9,16 @@ import org.elasticsearch.client.Client;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.util.ReflectionUtils;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Random;
 
 import static com.github.tlrx.elasticsearch.test.EsSetup.*;
 import static org.junit.Assert.assertEquals;
@@ -34,21 +28,24 @@ import static org.junit.Assert.assertEquals;
  * It supports data initialization thanks to the <a href="https://github.com/tlrx/elasticsearch-test">elasticsearch-test framework</a>.<br/>
  * It also performs generic CRUD tests on the tested repository.
  */
-//@RunWith(RandomizedRunner.class)
 @ContextConfiguration(classes = SpringDataEsTestConfiguration.class)
-public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R extends ElasticsearchRepository<T, ID>> {
+public abstract class InitializedSpringDataEsTestCase<T, ID extends Serializable, R extends ElasticsearchRepository<T, ID>> {
 
-    private final Class<T> typeClass;
-    private final Class<ID> idClass;
+    /**
+     * Document repository
+     */
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private R repository;
 
     /**
      * Elastic index of the document to test
      */
-    private String index;
+    private final String index;
     /**
      * Elastic type of the document to test
      */
-    private String type;
+    private final String type;
 
     /**
      * File extension for document mapping
@@ -69,13 +66,11 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
      */
     private EsSetup esSetup;
 
-    @Autowired
-    private R repository;
-
     /**
      * Logger
      */
     private static final Logger logger = LoggerFactory.getLogger(InitializedSpringDataEsTestCase.class);
+
 
     /*----------------------------------------------------------------------------------------------------------------*/
     /*                                      Initialization of Elastic index                                           */
@@ -86,14 +81,15 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
      */
     public InitializedSpringDataEsTestCase() {
 
-        // Find type, id and repository classes
+        // Find document class
         ParameterizedType superClass = (ParameterizedType) getClass().getGenericSuperclass();
         Type [] types = superClass.getActualTypeArguments();
-        typeClass = (Class<T>) types[0];
-        idClass = (Class<ID>) types[1];
+        @SuppressWarnings("unchecked")
+        Class<T> documentClass = (Class<T>) types[0];
 
-        index = typeClass.getDeclaredAnnotation(Document.class).indexName();
-        type = typeClass.getDeclaredAnnotation(Document.class).type();
+        // Get index and type from document class annotations
+        index = documentClass.getDeclaredAnnotation(Document.class).indexName();
+        type = documentClass.getDeclaredAnnotation(Document.class).type();
     }
 
     /**
@@ -110,6 +106,7 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
             createIndex.withMapping(getType(), mappingClassPath);
         }
         catch (EsSetupRuntimeException e) {
+            //noinspection ThrowableResultOfMethodCallIgnored
             logger.warn(ExceptionUtils.getRootCause(e).getMessage());
         }
 
@@ -123,6 +120,7 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
             esSetup.execute(deleteAll(), createIndex);
         }
         catch (EsSetupRuntimeException e) {
+            //noinspection ThrowableResultOfMethodCallIgnored
             logger.warn(ExceptionUtils.getRootCause(e).getMessage());
         }
     }
@@ -142,7 +140,7 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
     /*----------------------------------------------------------------------------------------------------------------*/
 
     /**
-     * Get the Elastic index
+     * Get the Elastic index.
      * @return The Elastic index
      */
     protected String getIndex() {
@@ -150,12 +148,18 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
     }
 
     /**
-     * Get the Elastic type
+     * Get the Elastic type.
      * @return The Elastic type
      */
     protected String getType() {
         return type;
     }
+
+    /**
+     * Give access to the tested repository.
+     * @return The tested repository
+     */
+    protected R getRepository() { return repository; }
 
     /**
      * Count the number of typed documents in the index.
@@ -167,47 +171,16 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
     }
 
     /**
-     * Instantiate a document and set its properties with random values
-     * Currently only support basic types and their object versions
-     * @return The new document
-     * @throws ReflectiveOperationException If the document couldn't be instantiated
+     * Return a new document for insertion.
+     * @return A document that will be inserted
      */
-    protected T newRandomDocument() throws ReflectiveOperationException {
-
-        T document = typeClass.newInstance();
-
-        Random random = RandomizedContext.current().getRandom();
-        ReflectionUtils.doWithFields(typeClass, field -> {
-            if (field.getType() == String.class)
-                field.set(document, RandomStrings.randomUnicodeOfLength(random, 50));
-            else if (field.getType() == int.class || field.getType() == Integer.class)
-                field.setInt(document, random.nextInt());
-            else if (field.getType() == long.class || field.getType() == Long.class)
-                field.setLong(document, random.nextLong());
-            else if (field.getType() == boolean.class || field.getType() == Boolean.class)
-                field.setBoolean(document, random.nextBoolean());
-            else if (field.getType() == double.class || field.getType() == Double.class)
-                field.setDouble(document, random.nextDouble());
-            else if (field.getType() == float.class || field.getType() == Float.class)
-                field.setFloat(document, random.nextFloat());
-            else if (field.getType() == byte.class || field.getType() == Byte.class)
-                field.setByte(document, (byte)random.nextInt());
-            else if (field.getType() == char.class || field.getType() == Character.class)
-                field.setChar(document, RandomStrings.randomAsciiOfLength(random, 1).charAt(0));
-            else if (field.getType() == short.class || field.getType() == Short.class)
-                field.setShort(document, (short)random.nextInt());
-            else
-                logger.warn("Unsupported object property : " + field.getName());
-        });
-
-        return document;
-    }
+    protected abstract T newDocumentToInsert();
 
     /**
-     * Give access to the tested repository
-     * @return The tested repository
+     * Return a new document for update.
+     * @return A document that will update an existing one
      */
-    protected R getRepository() { return repository; }
+    protected abstract T newDocumentToUpdate();
 
 
     /*----------------------------------------------------------------------------------------------------------------*/
@@ -232,7 +205,7 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
 
         assertEquals(2, countDocuments());
 
-        T toInsert = newRandomDocument();
+        T toInsert = newDocumentToInsert();
         T inserted = repository.index(toInsert);
 
         assertEquals(3, countDocuments());
@@ -247,7 +220,7 @@ public class InitializedSpringDataEsTestCase<T, ID extends Serializable, R exten
     public void indexExistingDocument() throws ReflectiveOperationException {
 
         assertEquals(2, countDocuments());
-        T toUpdate = newRandomDocument();
+        T toUpdate = newDocumentToUpdate();
         T updated = repository.index(toUpdate);
 
         assertEquals(2, countDocuments());
--- ws/src/test/java/info/jallaix/common/language/dao/LanguageDaoTest.java
+++ ws/src/test/java/info/jallaix/common/language/dao/LanguageDaoTest.java
@@ -2,13 +2,8 @@ package info.jallaix.common.language.dao;
 
 import info.jallaix.common.language.dto.Language;
 import org.junit.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
-
-import static org.junit.Assert.*;
 
 /**
  * The Language DAO must verify the following tests related to <b>language saving</b> :
@@ -34,44 +29,16 @@ public class LanguageDaoTest extends InitializedSpringDataEsTestCase<Language, S
 
 
     /*----------------------------------------------------------------------------------------------------------------*/
-    /*                                    Tests related to language indexing                                          */
+    /*                                   InitializedSpringDataEsTestCase overriden methods                            */
     /*----------------------------------------------------------------------------------------------------------------*/
 
-    /**
-     * Indexing a language must return a language with the same data
-     */
-    @Test(expected=IllegalArgumentException.class)
-    public void indexNullLanguage() {
-
-        getRepository().index(null);
+    @Override
+    protected Language newDocumentToInsert() {
+        return new Language("esp", "Español", "Spanish");
     }
 
-    /**
-     * Indexing a non existing language inserts the language in the index.
-     */
-    @Test
-    public void indexNewLanguage() {
-
-        assertEquals(2, countDocuments());
-
-        Language toInsert = new Language("esp", "Español", "Spanish");
-        Language inserted = getRepository().index(toInsert);
-
-        assertEquals(3, countDocuments());
-        assertEquals(toInsert, inserted);
-    }
-
-    /**
-     * Indexing an existing language replaces the language in the index.
-     */
-    @Test
-    public void indexExistingLanguage() {
-
-        assertEquals(2, countDocuments());
-        Language toUpdate = new Language("fra", "Español", "Spanish");
-        Language updated = getRepository().index(toUpdate);
-
-        assertEquals(2, countDocuments());
-        assertEquals(toUpdate, updated);
+    @Override
+    protected Language newDocumentToUpdate() {
+        return new Language("fra", "Español", "Spanish");
     }
 }
